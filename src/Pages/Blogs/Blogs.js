9b7e626e457601c9d6f6106f2a601fb22f334c13
SamaulHaque <samaulhaque749@gmail.com>
2022-11-29 01:37:57 +0600

    we deal in section added

--- src/Pages/Blogs/Blogs.js
+++ src/Pages/Blogs/Blogs.js
@@ -3,7 +3,46 @@ import React from 'react';
 const Blogs = () => {
     return (
         <div>
-            blogs
+            <div tabIndex={0} className="collapse collapse-arrow border border-base-300 bg-base-100 rounded-box">
+                <div className="collapse-title text-xl font-medium">
+                    What are the different ways to manage a state in a React application?
+                </div>
+                <div className="collapse-content">
+                    <p> <b>URL: </b> Keeping such data in the URL allows users to share deep links with others.It is recommended to avoid storing such information in the app’s state to avoid the URL in our app getting out of sync. The URL should be used as the system of record, Read from it as needed for information related to sorting, pagination, etc.</p> <br />
+
+                    <p><b>Web Storage</b> The second option is to store the state in the browser via web storage. This is useful when we want to persist state between reloads and reboots. Examples include cookies, local storage, and IndexedDB. These are native browser technologies. Data persisted in the browser is tied to a single browser. So, if the user loads the site in a different browser, the data will not be available.</p> <br />
+
+                    <p><b>Local State</b> The third option is to use store state locally. It is useful when one component needs the state.</p>
+                </div>
+            </div>
+            <div tabIndex={1} className="collapse collapse-arrow border border-base-300 bg-base-100 rounded-box">
+                <div className="collapse-title text-xl font-medium">
+                    How does prototypical inheritance work?                </div>
+                <div className="collapse-content">
+                    <p>
+                        Prototypal inheritance
+                        In programming, we often want to take something and extend it.For instance, we have a user object with its properties and methods, and want to make admin and guest as slightly modified variants of it. We’d like to reuse what we have in user, not copy/reimplement its methods, just build a new object on top of it.</p>
+                </div>
+            </div>
+            <div tabIndex={2} className="collapse collapse-arrow border border-base-300 bg-base-100 rounded-box">
+                <div className="collapse-title text-xl font-medium">
+                    What is a unit test? Why should we write unit tests?
+                </div>
+                <div className="collapse-content">
+                    <p>
+                        <b>Unit Test: </b> Unit testing is a software development process in which the smallest testable parts of an application, called units, are individually and independently scrutinized for proper operation. This testing methodology is done during the development process by the software developers and sometimes QA staff.  The main objective of unit testing is to isolate written code to test and determine if it works as intended.
+                        Unit testing is an important step in the development process, because if done correctly, it can help detect early flaws in code which may be more difficult to find in later testing stages.
+                    </p> <br />
+                </div>
+            </div>
+            <div tabIndex={0} className="collapse collapse-arrow border border-base-300 bg-base-100 rounded-box">
+                <div className="collapse-title text-xl font-medium">
+                    Focus me to see content
+                </div>
+                <div className="collapse-content">
+                    <p>tabIndex={0} attribute is necessary to make the div focusable</p>
+                </div>
+            </div>
         </div>
     );
 };
