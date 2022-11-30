421fe6a26cb975dba9467e8a66c6c20b406344aa
SamaulHaque <samaulhaque749@gmail.com>
2022-12-01 00:09:14 +0600

    booking modal created

--- src/Pages/Categories/CategoryMobile.js
+++ src/Pages/Categories/CategoryMobile.js
@@ -1,20 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useLoaderData } from 'react-router-dom';
+import BookingModal from './BookingModal/BookingModal';
 import CategoryMobileCard from './CategoryMobileCard';
 
 const CategoryMobile = () => {
     const categoryMobile = useLoaderData();
+    const [bookPhone, setBookPhone] = useState(null)
     return (
         <div>
-            <h2 className='text-4xl text-cyan-600 text-center font-bold mb-16'>This Category Mobile Phones Here.</h2>
+            <h2 className='text-3xl text-cyan-600 text-center font-bold mb-16'>This Category Mobile Phones Here.</h2>
         <div className='grid gap-8 grid-cols-3 mx-5'>
             {
                 categoryMobile.map(mobile => <CategoryMobileCard
                 key={mobile._id}
                 mobile={mobile}
+                setBookPhone={setBookPhone}
                 ></CategoryMobileCard>)
             }
         </div>
+        {
+            bookPhone &&
+            <BookingModal
+            bookPhone={bookPhone}
+            ></BookingModal>
+        }
         </div>
     );
 };
--- src/Pages/Categories/CategoryMobileCard.js
+++ src/Pages/Categories/CategoryMobileCard.js
@@ -1,16 +1,27 @@
 import React from 'react';
 
-const CategoryMobileCard = ({ mobile }) => {
-    const { title, img } = mobile;
+const CategoryMobileCard = ({ mobile, setBookPhone}) => {
+    const { title, img, location, resale_price, original_price, Used, posted_time, seller_name } = mobile;
     return (
         <div>
             <div className="card w-96 bg-base-100 shadow-xl">
                 <figure><img src={img} alt="mobile-img" className='h-80' /></figure>
                 <div className="card-body">
                     <h2 className="card-title">{title}</h2>
-                    <p></p>
+                    <p>
+                    <span>Original Price: <b>{original_price}</b> Taka</span> <br />
+                    <span>Resale Price: <b>{resale_price}</b> Taka</span> <br />
+                    <span>Used: <b>{Used}</b></span> <br />
+                    <span>Posted Time: <b>{posted_time}</b></span> <br />
+                    <span>Location: <b>{location}</b></span> <br />
+                    <span>Seller Name: <b>{seller_name}</b></span>
+                     </p>
                     <div className="card-actions justify-end">
-                        <button className="btn btn-primary">Book Now</button>
+                        <label
+                        onClick={()=>setBookPhone(mobile)}
+                        htmlFor="booking-modal"
+                         className="btn"
+                         >Book Now</label>
                     </div>
                 </div>
             </div>
